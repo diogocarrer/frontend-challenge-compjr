ff2b9ae663755394d9c2f1763382441719229609
diogocarrer <diogocarrer14@gmail.com>
2024-05-31 09:54:12 -0300

    update

--- src/pages/SaibaMais.jsx
+++ src/pages/SaibaMais.jsx
@@ -1,5 +1,6 @@
 /* assets */
-import Sobre from "../assets/saiba_mais.svg";
+import Servicos from "../assets/servicos.svg";
+import Sobre from "../assets/compjunior.jpg";
 /* react */
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
@@ -25,11 +26,21 @@ const SaibaMais = () => {
 
     fetchData();
 
+    window.scrollTo(0, 0);
   }, [id]);
+
+  const renderImage = (id) => {
+    switch (id) {
+      case 1:
+        return <img src={Sobre} alt="Sobre" className={styles.imagem_sobre} />;
+      default:
+        return <img src={Servicos} alt="Serviços" className={styles.imagem_service} />;
+    }
+  };
 
   return (
     <div className={styles.pagina_sobre}>
-      <img src={Sobre} alt="Sobre" className={styles.imagem_sobre} />
+      {pagina && renderImage(pagina.id)}
       {pagina && (
         <div>
           <h1 className={styles.titulo_sobre}>{t(pagina.titulo_sobre)}</h1>
